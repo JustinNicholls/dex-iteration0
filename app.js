be7e3a0eded2b30f729427990d3d1aabcb8b76eb
Chun-Kwan Alex Fung <cfung58@uwo.ca>
2014-11-18 20:33:31 -0500

    Commit, now with function that gets contest duration

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ function content_playlist(res,req){
 			//requires to be encoded in utf8 
 			res.setEncoding('utf8');
 			
-			res.on('data',function(theRet){
+			res.on('data',function(res2){
 				console.log("Content playlist triggered");
 			});
 		});
@@ -72,6 +72,65 @@ function content_playlist(res,req){
 
 //testing the contentplaylist function 
 //content_playlist(); 
+
+//query list: 
+var start_query = "UPDATE contest_sc_intelligence SET current_msg=(SELECT starting_msg FROM contest_configuration), game_state = 'started'";
+var end_query = "UPDATE contest_sc_intelligence SET current_msg=(SELECT final_msg FROM contest_configuration), game_state = 'final'";
+var choose_winner_query= "UPDATE contest_sc_intelligence SET game_state = 'winner'";
+var contest_duration_query = "SELECT contest_duration FROM contest_configuration";
+var winner_selection_duration_query = "SELECT winner_selection_duration FROM contest_configuration";
+
+//start the content 
+app.post('/content', function(res, req){
+		var datastore = res.body.datastore; //get datastore 
+		var contest_duration; //contest duration time that will be retrieved from database
+		var winner_selection_duration; //winner_selection_duration that will be retrieved from database 
+		
+		
+		//create new time that will be the time needed to schedule the start 
+		var start_time = new Date();
+		//set hour and minutes here 
+		var hours = 15;
+		var minutes = 30;
+		start_time.setHours(hours);
+		start_time.setMinutes(minutes);
+
+		//get contest_duration 
+		var get_contest_duration = schedule.scheduleJob(start_time, function(){
+			console.log("Getting contest duration at: " + start_time); 
+			
+			var get_contest_duration_options={
+				host: 'developer.kb.dexit.co',
+				port: 80,
+				path: '/access/stores/'+datastore+'/query/?query='+encodeURIComponent(contest_duration_query), //encode query 
+				method: 'GET',
+				headers: { 
+					'Authorization' : 'Bearer '+ tokenString,
+					'Accept' : 'application/json',
+					'Name' : 'Value'
+				}
+			};  
+			
+			//http request 
+			req = http.request(get_contest_duration_options,function(res){
+				//encode in utf8 
+				res.setEncoding('utf8');
+			
+				res.on('data',function(res2){
+					var contest_duration_message = JSON.parse(res2);
+					console.log("Contest Duration = " + contest_duration_message.result.rows[0][0]); //get the contest duration from the query 
+					contest_duration = contest_duration_message.result.rows[0][0];
+				});
+			});
+		
+		//end request 	
+		req.end();			
+			
+		}); //end of get_contest_duration 
+		
+		
+		
+	}); 
 
 
 app.use('/client', express.static('public'));
