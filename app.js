fa7553bed5d4b265b35d87b5bba9cb69cd9582a4
Chun-Kwan Alex Fung <cfung58@uwo.ca>
2014-11-18 19:20:20 -0500

    Second commit with content_playlist function added

--- app.js
+++ app.js
@@ -45,6 +45,35 @@ access.authenticate("cfung58@uwo.ca", "Beaufighter_1", "activesight", function(e
 	 console.log("Token: " , token);
 }); 
 
+//function for contentplaylist (post) 
+function content_playlist(res,req){
+	var content_playlist_options = {
+			host: 'developer.kb.dexit.co',
+			port: 80,
+			path: '/events/bb0f3a80-bcb2-4387-b339-3439554ce0f1/trigger',
+			method: 'POST',
+			headers: { 
+				'X-Auth-Key' : accessTokenString
+			}
+		};
+		
+		//send the request with the options 
+		req = http.request(content_playlist_options,function(res){
+			//requires to be encoded in utf8 
+			res.setEncoding('utf8');
+			
+			res.on('data',function(theRet){
+				console.log("Content playlist triggered");
+			});
+		});
+
+		req.end();
+}
+
+//testing the contentplaylist function 
+//content_playlist(); 
+
+
 app.use('/client', express.static('public'));
 
 ////////////
